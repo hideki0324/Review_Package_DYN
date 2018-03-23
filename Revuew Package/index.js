5ab54bca11d6aa1a3537174bc04babf044615264
hideki0324 <hideki19850324@gmail.com>
2018-03-23 06:49:11 -0700

    Add files via upload
    1st Upload

---
+++ Revuew Package/index.js
@@ -0,0 +1,15 @@
+//LEVEL 1 - BASIC HTML / CSS
+document.getElementById("menu").addEventListener("click", function(){
+    document.getElementById("controls").style.bottom = "-400px";
+});
+
+
+//LEVEL 2 - SIMPLE JS
+document.getElementById("menu").addEventListener("click", function(){
+    document.getElementById("controls").style.bottom = "-400px";
+});
+
+var imgwidth = 100;
+var imgheight = 70;
+var zoomimg = document.getElementById("zoom").style.backgroundImage;
+var ch1back = document.getElementById("ch1").style.backgroundImage.value;
